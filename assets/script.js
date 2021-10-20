9e70dd821ae491e2b96047ce100e59c3d98573cb
James Sgarella <Jimbo8702@gmail.com>
2021-10-19 20:51:43 -0400

    added all questions, sized buttons correctly

--- assets/script.js
+++ assets/script.js
@@ -6,50 +6,52 @@ var answerFour = document.getElementById("answers4");
 var question = document.getElementById("Question");
 var timer = document.getElementById("timerBox");
 var startButton = document.getElementById("StartButton");
+
 //DATA
+var index = 0;
 var time = 30;
 var score;
 var highscore;
 const answerLetter = ["A", "B", "C", "D"];
 var questionOne = {
-  Question: "select 2",
-  answer1: "1",
-  answer2: "2",
-  answer3: "3",
-  answer4: "4",
-  correct: "2",
+  Question: "The coding language that styles an HTML page",
+  answer1: "CSF",
+  answer2: "CSS",
+  answer3: "SSC",
+  answer4: "CCS",
+  correct: "CSS",
 };
 var questionTwo = {
-  Question: "",
-  answer1: "",
-  answer2: "",
-  answer3: "",
-  answer4: "",
-  correct: "",
+  Question: "Which sets a variable in JavaScript",
+  answer1: "var",
+  answer2: "make",
+  answer3: "set",
+  answer4: "assign",
+  correct: "var",
 };
 var questionThree = {
-  Question: "",
-  answer1: "",
-  answer2: "",
-  answer3: "",
-  answer4: "",
-  correct: "",
+  Question: "Which is used to close a line in JavaScript",
+  answer1: "semi colon",
+  answer2: "square brackets",
+  answer3: "curly brackets",
+  answer4: "forward slash",
+  correct: "semi colon",
 };
 var questionFour = {
-  Question: "",
-  answer1: "",
-  answer2: "",
-  answer3: "",
-  answer4: "",
-  correct: "",
+  Question: "Which data type stores properties",
+  answer1: "boolean",
+  answer2: "arrays",
+  answer3: "objects",
+  answer4: "integer",
+  correct: "objects",
 };
 var questionFive = {
-  Question: "",
-  answer1: "",
-  answer2: "",
-  answer3: "",
-  answer4: "",
-  correct: "",
+  Question: "What is the correct notation to select an id named target in CSS",
+  answer1: ":target",
+  answer2: ".target",
+  answer3: "-target",
+  answer4: "#target",
+  correct: "#target",
 };
 const questionList = [
   questionOne,
@@ -61,12 +63,25 @@ const questionList = [
 //FUNCTIONS
 function startGame() {
   totalTime();
-  presentNextQuestion();
+  presentNextQuestion(index);
   startButton.disabled = true;
-  //starts count down
-  //first question appears
 }
-function checkCorrect(event, i) {
+function checkCorrect() {
+  questionContainer.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches("button")) {
+      var correctAnswer = questionList[index].correct;
+      if (element.textContent === correctAnswer) {
+        index++;
+        console.log("It worked");
+        if (index > questionList.length) {
+          quizOver();
+        }
+        presentNextQuestion(index);
+      }
+    }
+  });
+
   //check if selected answer matches desired
   //if not correct, take 5 seconds off time
 }
@@ -74,17 +89,13 @@ function setHighScore() {
   //take the score and store it to local storage
   //present it on a highscore list
 }
-function presentNextQuestion(i) {
-  console.log(questionList[i].Question);
-  question.textContent = questionList[i].Question;
-  answerOne.textContent = questionList[i].answer1;
-  answerTwo.textContent = questionList[i].answer2;
-  answerThree.textContent = questionList[i].answer3;
-  answerFour.textContent = questionList[i].answer4;
-  //grab a question
-  //present on page
-  //select in order from array
-  //start at index 0 and add 1 everytime correct
+function presentNextQuestion(index) {
+  console.log(questionList[index].Question);
+  question.textContent = questionList[index].Question;
+  answerOne.textContent = questionList[index].answer1;
+  answerTwo.textContent = questionList[index].answer2;
+  answerThree.textContent = questionList[index].answer3;
+  answerFour.textContent = questionList[index].answer4;
 }
 function totalTime() {
   var timerInterval = setInterval(function () {
